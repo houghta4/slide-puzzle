6f30fe464a0452cd760735c1e23faff28c43b2da
Vince <houghta4@uwm.edu>
2023-10-14 00:13:38 -0600

    cut image function

--- puzzle.js
+++ puzzle.js
@@ -13,6 +13,10 @@ let imgVec = [1, 2, 3, 4, 5, 6, 7, 9, 8];
     - Variable amounts of rows and cols
 */
 window.onload = () => {
+    let image = new Image();
+    image.onload = () => cutUpImage(image);
+    image.src = 'img/2.jpg';
+
     let _imgVec = [...imgVec];
     for (let i = 0; i < ROWS; i++) {
         for (let j = 0; j < COLS; j++) {
@@ -28,7 +32,7 @@ window.onload = () => {
             tile.setAttribute("draggable", false);
 
             tile.addEventListener("click", onTileClick);
-            
+
             document.getElementById("game-board").append(tile);
         }
     }
@@ -45,7 +49,7 @@ let onTileClick = (e) => {
         incTurns();
     } else {
         document.getElementById("game-board").classList.add("shake");
-        setTimeout(function() {
+        setTimeout(function () {
             document.getElementById("game-board").classList.remove("shake");
         }, 500);
     }
@@ -63,4 +67,31 @@ let validateMove = (tileId) => {
 
 let incTurns = () => {
     document.getElementById("turns").innerText = ++turns;
+}
+
+let cutUpImage = (image) => {
+    let imagePieces = [];
+    let widthOfOnePiece = image.width / ROWS;
+    let heightOfOnePiece = image.height / COLS;
+    for (let y = 0; y < ROWS; y++) {
+        for (let x = 0; x < COLS; x++) {
+            let canvas = document.createElement('canvas');
+            canvas.width = widthOfOnePiece;
+            canvas.height = heightOfOnePiece;
+            let context = canvas.getContext('2d');
+            context.drawImage(image, x * widthOfOnePiece, y * heightOfOnePiece, widthOfOnePiece, heightOfOnePiece, 0, 0, canvas.width, canvas.height);
+            imagePieces.push(canvas.toDataURL());
+        }
+    }
+
+    // imagePieces now contains data urls of all the pieces of the image
+
+    let container = document.getElementById('cut-container');
+    for (let i = 0; i < imagePieces.length; i++) {
+        let newImage = document.createElement('img');
+        newImage.src = imagePieces[i];
+        newImage.style.paddingRight = "2px";
+        container.appendChild(newImage);
+    }
+    // console.table(imagePieces);
 }
